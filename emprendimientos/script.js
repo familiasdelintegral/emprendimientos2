37930cf182a79b79e4edfed7af0ced8db6dda682
Familias del Integral <familias.integral@integralnuevosayres.esc.edu.ar>
2026-07-23 17:47:35 -0300

    Add files via upload

--- emprendimientos/script.js
+++ emprendimientos/script.js
@@ -716,11 +716,7 @@ const CONSOLIDATED_ENTRIES = [
     nombre: 'Ezequiel Smiriglia',
     sala: 'Azul, Rosa',
     rubro: '🍽️ Gastronomía y viajes',
-    descripcion: '🥑 Avocados de especialidad
-☕ Café & brunch all day
-🌿 Todos los días de 8 a 20 hs
-📍 Palermo
-Soler 5999, Buenos Aires, Argentina 1425',
+    descripcion: 'Empresario gastronómico',
     whatsapp: '5491165538174',
     instagram: 'theavocadocompany.arg',
     webs: [],
